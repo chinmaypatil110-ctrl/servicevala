a02146ed8adecf4b73546e8855f0667755bd1674
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-06-04 23:17:56 +0530

    Add database connection and update .gitignore

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 import path from 'path';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
+import db from './db/connection.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
